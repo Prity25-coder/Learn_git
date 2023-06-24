622ffcf8dc55dcce16e34de5fac475730ca5b8ba
Prashant Kumar <pktherock00@gmail.com>
2023-06-24 16:10:40 +0530

    hotel reservation report completed (#2)
    * hotel reservation report completed
    
    * checking reveiwer resetting after new commit push

---
+++ js/app.js
@@ -0,0 +1,7 @@
+// We are learning git and github flow
+
+console.log('hello GitHub!!');
+
+console.log('I have completed hotel reservation report');
+
+console.log('Now Pr will automatically reset for reviewers');
